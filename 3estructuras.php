d8376d45935bf0f664e70b68600aeb387c406d8b
Aldo Daniel <aldo.lara@coppel.com>
2024-03-06 18:41:26 -0600

    git commit -m 'resolucion de conflicto'

--- 3estructuras.php
+++ 3estructuras.php
@@ -1,7 +1,17 @@
+<?php
 //listas
 $entero = 8;
-$duble = 8.02
+$duble = 8.02;
 $boolean = true;
 $string = "Cadena de texto";
 
-$varLista = [$entero, $double, $boolean, $string]
+$varLista = [$entero, $double, $boolean, $string];
+echo gettype($varLista);    //array
+echo $varLista[0];      // 8
+echo $varLista[3];      // Cadena de texto
+
+// añadimos un nuevo elemento al array
+$nuevoElemento = 'Carne Asada';
+array_push($varLista, $nuevoElemento); 
+echo $varLista;
+?>
