8b1e965d47ab2a335838dfb42f052c26ade8a554
Aldo Daniel <aldo.lara@coppel.com>
2024-03-06 18:37:29 -0600

    Update file 3estructuras.php

--- 1.php
+++ 1.php
@@ -1,40 +1,24 @@
-<?PHP
-//comenetario una linea 
-echo "Hola PHP";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <?PHP
+        $salto = "\n";
 
-/*
-Comentario
-en
-varias
-lineas
-*/
+        echo "Hola PHP \n" . $salto;
 
-//Variables
-$variableString = "Esto es una cadena de texto";
-echo $variableString;
+        $variableString = "Esto es una cadena de texto";
+        echo $variableString . $salto;
 
-$texto1 = "texto para";
-$texto2 = "cocatenar";
-$texto2 = " que cambia su info";
-echo $texto1 . $texto2;
+        $texto1 = "texto para";
+        $texto2 = " cocatenar";
+        $texto2 = " que cambia su info";
+        echo $texto1 . $texto2;
+    ?>
+</body>
+</html>
 
-echo gettype($texto2);  // imprime el tipo de variable que es
-
-$entero = 6;
-$entero = 8;
-echo $entero; //8
-echo gettype($entero);   //Integer (entero)
-
-$decimal = 4.5;
-echo $decimal;  //4.5
-
-$booleano = true;
-echo $booleano; // 1
-echo gettype($booleano); //boolean
-
-$booleano = false;
-echo $booleano;     // (  )      -> no imprime nada o lo que igual a false
-echo gettype($booleano); //boolean
-
-echo "El valor de mi entero es: $entero y en valor de mi decimal es: $decimal , asi como el valor de mi booleano es:$boolean ";
-?>
