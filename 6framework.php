4ab9614af505129239255b7fa6bb6db17bb8eaab
Aldo Daniel <aldo.lara@coppel.com>
2024-03-20 10:32:53 -0600

    Correcion 6framework.php

--- 6framework.php
+++ 6framework.php
@@ -1,6 +1,6 @@
 <?php
 // un framework que se puede usar y/o aprender
-// el cual se lleva con php es: Larabel
+// el cual se lleva con php es: Laravel
 // Laravel es un framework de código abierto para desarrollar aplicaciones y servicios 
 // web con PHP 5, PHP 7 y PHP 8. Su filosofía es desarrollar código PHP de forma 
 // elegante y simple, evitando el "código espagueti".
