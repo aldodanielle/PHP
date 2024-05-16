b5bbe0b9ccc49a8fc72f2da9dfbddb5ee24e3a69
Aldo Daniel <aldo.lara@coppel.com>
2024-05-15 18:37:17 -0600

    envio.php is updated

--- 7variables/envio.php
+++ 7variables/envio.php
@@ -5,5 +5,25 @@ echo $_POST['user'];
 echo $_REQUEST['user'];
 
 
-echo 'Hola' . htmlspecialchars($_GET["nombre"]) . "!";
+//echo 'Hola' . htmlspecialchars($_GET["nombre"]) . "!";
 ?>
+
+<!-- variables en formulario mas complejos -->
+<?php
+if($_POST){
+    echo '<pres>';
+    echo htmlspecialchars(print_r($_POST, true));
+    echo '</pres>';
+}
+?>
+
+<form action="" method="post">
+    Nombre: <input type="text" name="personal[nombre]" /> <br/>
+    Email: <input type="text" name="personal[emal]"/><br/>
+    Sexo: <br/>
+        <select multiple name="sexo[]">
+            <option value="masculino">Masculino</option>
+            <option value="femenino">Femenino</option>
+        </select> <br/>
+        <input type="submit" value="Enviar">
+</form>
