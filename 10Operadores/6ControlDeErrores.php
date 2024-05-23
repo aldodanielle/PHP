6601c1c919bba744ea7c7eafcdb0f7052bb8fad6
Aldo Daniel Lara Emiliano <aldo.lara@coppel.com>
2024-05-23 17:13:38 -0600

    update

--- 10Operadores/6ControlDeErrores.php
+++ 10Operadores/6ControlDeErrores.php
@@ -1,4 +1,24 @@
 <?php
 //          ---+++++     Operador de control de Erorres     +++++---
 
+// PHP cuenta con un soporte de operador de errores "el signo de @"
+// cuando se antepone a una expresion en PHP , cualquier mensaje de error pueden ser generado por esa expresion sera ignorado
+
+// algunas funciones para errores 
+// set_error_handler    -> https://www.php.net/manual/es/function.set-error-handler.php
+// error_repoting       -> https://www.php.net/manual/es/function.error-reporting.php
+// este ultimo es para u error mas personalizado
+
+
+
+// Ej 
+// Error intencional de un archivo 
+$php_errormsg = __FILE__;
+$my_file = @file ('El archivo no existe') or
+        die ("La apertura del archivo a fallado: error '$php_errormsg'");
+
+// esta funciona con cualquier expresion, no solo con funciones 
+$key = "valor";
+$value = @$cahe[$key];
+echo $value;
 ?>
