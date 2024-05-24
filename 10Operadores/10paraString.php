ba9cfc7a269a88c2a3efabd84bc5f74d6ef475af
Aldo Daniel Lara Emiliano <aldo.lara@coppel.com>
2024-05-24 16:26:09 -0600

    update

--- 10Operadores/10paraString.php
+++ 10Operadores/10paraString.php
@@ -1,3 +1,16 @@
 <?php
+//          ---+++++     Operadores para string     +++++---
 
+// contamos con 2 operadores
+// Concatenacion ('.')
+// Sobre concatenacion ('.=') para cuando no queremos perder la informacion al sobreescribir una variable
+
+
+$name = " Danielle ";
+$lastname = $name . " Lara ";
+echo $lastname;         // Danielle Lara
+
+$fullname = " Danielle";
+$fullname .= " Lara";    
+echo $fullname;         // Danielle Lara
 ?>
