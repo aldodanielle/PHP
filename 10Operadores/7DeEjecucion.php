57e50673b6f01c2fcd9c8ccc3dd149a270b3ab9c
Aldo Daniel Lara Emiliano <aldo.lara@coppel.com>
2024-05-23 18:32:14 -0600

    update

--- 10Operadores/7DeEjecucion.php
+++ 10Operadores/7DeEjecucion.php
@@ -1,3 +1,16 @@
 <?php 
+//          ---+++++     Operador de control de Ejecucion     +++++---
+// ``
+// no son mas que las comillas invertidas
+$output = `ls -al`;
+echo "<pre>$output</pre>";
 
+
+$credito = '20k';
+$saldo = '10k';
+echo `tu credito es {$credito} y tienes de saldo {$saldo}`;
+
+
+$host = 'www.google.com';
+echo `ping -n 3 {$host}`;
 ?>
