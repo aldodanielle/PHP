418d140b141c4056a2e2d9c2467ceb31698b9605
Aldo Daniel Lara Emiliano <aldo.lara@coppel.com>
2024-05-24 13:32:21 -0600

    update

--- 10Operadores/9logicos.php
+++ 10Operadores/9logicos.php
@@ -1,3 +1,29 @@
 <?php
+//          ---+++++     Operador Logicos     +++++---
 
+$a and $b;      // And (y)              -> true si tanto $a como $b son true
+$a or $b;       // or (o inclusivo)     -> true si cualquiera de $a o $b es true
+$a xor $b;      // Xor (o exlusivo)     -> true si $a o $b es true pero no ambos 
+!$a;            // not (no)             -> si $a no es true
+$a && $b;       // and (y)              -> si tanto $a como &b son true 
+$a || $b;       // or (o inclusivo)     -> si cualquiera de $a o $ es true
+
+
+
+
+// por lo menos uno es verdadero
+$e = false || true;
+// por lo menos uno es verdadero
+$f = false or true;
+
+var_dump($e, $f);
+
+
+
+// los dos tiene que ser verdaderos
+$g = true && false;
+// los dos tiene que ser verdaderos
+$h = true and false;
+
+var_dump($g, $h);
 ?>
