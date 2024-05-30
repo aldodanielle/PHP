20ab6189a50748e7ed9cb8add79b912354690826
Aldo Daniel Lara Emiliano <aldo.lara@coppel.com>
2024-05-30 16:56:10 -0600

    update: the file is updated

--- 11EstructurasDeControl/7do_while.php
+++ 11EstructurasDeControl/7do_while.php
@@ -1,3 +1,35 @@
 <?php
+//          ---+++++     do while     +++++---
 
+// this strcuture se verifica al final de la iteracion, es decir, que por lo menos se realiza uno vez
+$i = 2;
+do{
+    echo " i es $i \n";
+    echo " i es mayor a 3";
+}while($i > 3);     // se ejecuta solo una vez porque $i no es mayor que 3
+
+
+$f = 0;
+do{
+    echo " f es $f \n";
+    echo " f hora es $f por lo tanto menor a 3 \n";
+    $f++;
+}while($f < 3);     // se ejecuta varias veces(3) hasta que se cumpla la condicion 
+
+
+// concepto de encapsular do-while(0)
+$i = 2;
+do{
+    if ($i < 5){
+        echo "i no es suficientemente grande";
+        break;
+    }
+    $i *= $factor;
+    if($i < $minimum_limit){
+        break;
+    }
+    echo "i es suficiente";
+
+    /* Procesa i*/
+}while(0);
 ?>
