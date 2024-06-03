fa1f17db405869c9e2848ba776d3512266c12b8d
Aldo Daniel Lara Emiliano <aldo.lara@coppel.com>
2024-06-03 17:48:36 -0600

    the file is update and ready

--- 11EstructurasDeControl/11continue.php
+++ 11EstructurasDeControl/11continue.php
@@ -1,3 +1,37 @@
 <?php
+//          ---+++++     Continue     +++++---
 
+// utilizado para saltar la iteneracion actual, para terminar con el flujo antes de pasar
+// a la siguiente iteneracion
+// en switch es el mismo procedimieno que el break y tambien con la enumeracion continue 1, continue 2
+while(list($clave, $valor) = each($array)){
+    if(!($clave % 2)){  // salta los miembros pares
+        continue;
+    }
+    hace_algo($valor);
+}
+
+
+$i = 0;
+while($i++ < 5){
+    echo "Exterior \n";
+    while(1){
+        echo "medio \n";
+        while(1){
+            echo "Exterior \n";
+            continue 3;
+        }
+        echo "Never se imprime \n";
+    }
+    echo "this neither (tampoco) \n";
+}
+
+
+
+// para -> for
+for($i = 0; $i < 5; ++$i){
+    if($i == 2)
+        continue
+    print $i . "\n";
+}
 ?>
