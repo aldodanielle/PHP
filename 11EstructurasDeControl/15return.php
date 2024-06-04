91d39aea803a0b082d7b8fa4e4b18096b513008d
Aldo Daniel Lara Emiliano <aldo.lara@coppel.com>
2024-06-04 17:45:52 -0600

    the file is updated

--- 11EstructurasDeControl/15return.php
+++ 11EstructurasDeControl/15return.php
@@ -1,5 +1,8 @@
 <?php
+//          ---+++++     return     +++++---
 
-
+// devuelve el control del programa al modulo que lo invoca
+// la ejecucion vuelve a la siguiente expresion despues del modulo que lo invoca 
+// es llamado desde una funcion y donde return termina inmediatamente la ejecucion 
 
 ?>
