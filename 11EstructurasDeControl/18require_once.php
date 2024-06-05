4f8c8dc8bb66673298235f2a115dfe2e7828d388
Aldo Daniel Lara Emiliano <aldo.lara@coppel.com>
2024-06-05 12:26:18 -0600

    the file was updated and ready

--- 11EstructurasDeControl/18require_once.php
+++ 11EstructurasDeControl/18require_once.php
@@ -1,5 +1,12 @@
 <?php
+//          ---+++++     require_once     +++++---
+
+
+// es identica a require expecto que PHP verifica que si el archivo ya fue incluido
+// y si es asi, no se incluye (require) de nuevo 
 
 
 
+// es muy utili para cuando el archivo se necesita ser incluido mas de una vez para su evaluacion 
+// asi que esto puede ser de gran ayuda 
 ?>
